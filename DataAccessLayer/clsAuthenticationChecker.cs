481ec8565b3649465f6fdbd2a8f7f3f7ab6d17dd
WaelKendouli <kendouliwael@gmail.com>
2026-09-14 18:19:16 +0200

    Adding the connection string plus checking authentication

---
+++ DataAccessLayer/clsAuthenticationChecker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class clsAuthenticationChecker
+    {
+        public static bool CheckAuthentication(string username, string password)
+        {
+            using (SqlConnection conx = new SqlConnection(clsConnection.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("SP_CheckAuthentication", conx))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                try
+                {
+                    conx.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // If any row is returned, the credentials are valid
+                        return reader.HasRows;
+                    }
+                }
+                catch (Exception ex)
+                {
+                   // Handle exception (log it, etc.)
+                    return false;
+                }
+            }
+        }
+
+    }
+}
