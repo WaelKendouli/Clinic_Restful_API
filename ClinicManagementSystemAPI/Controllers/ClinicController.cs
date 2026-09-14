32832eafadb8c4f061d560a8adc03ea990e83245
WaelKendouli <kendouliwael@gmail.com>
2026-09-14 20:24:15 +0200

    Adding secondary Authentication method

--- ClinicManagementSystemAPI/Controllers/ClinicController.cs
+++ ClinicManagementSystemAPI/Controllers/ClinicController.cs
@@ -7,12 +7,34 @@ namespace ClinicManagementSystemAPI.Controllers
     [ApiController]
     public class ClinicController : ControllerBase
     {
-        [HttpGet (Name = "AuthenticateUser")]
+        [HttpGet ("auth" ,Name = "AuthenticateUser")]
         public IActionResult AuthenticateUser(string username, string password, string phone = "", string email = "")
         { 
             try
             {
                 LoggingAuthenticator authenticator = new LoggingAuthenticator(username, password, phone, email);
+                bool isAuthenticated = authenticator.Authenticate();
+                if (isAuthenticated)
+                {
+                    return Ok(new { message = "Authentication successful" });
+                }
+                else
+                {
+                    return Unauthorized(new { message = "Authentication failed" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+        [HttpGet("Sec-auth" ,Name = "AuthenticateUserWithSecondaryMethod")]
+        public IActionResult AuthenticateUserSecondaryMethod( string phone , string email )
+        {
+            try
+            {
+                LoggingAuthenticator authenticator = new LoggingAuthenticator("", "", phone, email);
+                authenticator.SwitchToSecondaryAuthenticationMethod();
                 bool isAuthenticated = authenticator.Authenticate();
                 if (isAuthenticated)
                 {
