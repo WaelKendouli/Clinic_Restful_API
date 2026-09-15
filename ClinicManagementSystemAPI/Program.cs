c2fc2330e043b704b0d30bf78d27b8777e2f467b
WaelKendouli <kendouliwael@gmail.com>
2026-09-15 14:59:53 +0200

    Trying to get over cross-origin policy

--- ClinicManagementSystemAPI/Program.cs
+++ ClinicManagementSystemAPI/Program.cs
@@ -5,7 +5,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); // <--- Make sure this is here!
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+
 
 // 2. ENABLE SWAGGER MIDDLEWARE
 if (app.Environment.IsDevelopment())
@@ -13,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();   // Generates the JSON spec
     app.UseSwaggerUI(); // Serves the HTML webpage at /swagger
 }
+
+app.UseCors("AllowFrontend");   // <-- MUST be before MapControllers / UseAuthorization
+
 
 app.UseAuthorization();
 app.MapControllers();
