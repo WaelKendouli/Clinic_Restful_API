ca5377120139e307c22ad9b0b8acfe24890628dd
WaelKendouli <kendouliwael@gmail.com>
2026-09-15 11:17:34 +0200

    Adding DTO layer to the project

---
+++ DTOsLayer/LoginDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOsLayer
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Email { get; set; } = "";
+        public LoginDTO(string username, string password, string phone = "", string email = "")
+        {
+            Username = username;
+            Password = password;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}
