b129b1b2b8a3399d3a9fe4e7334693ed4c2cb6cf
WaelKendouli <kendouliwael@gmail.com>
2026-09-17 13:46:00 +0200

    setting properties for DTO class doctor

--- DTOsLayer/DoctorDTO.cs
+++ DTOsLayer/DoctorDTO.cs
@@ -8,6 +8,33 @@ namespace DTOsLayer
 {
     public class DoctorDTO
     {
+        public int DoctorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public string PhotoURL { get; set; }
+        public int SpecializationID { get; set; }
 
+       
+
+        public DoctorDTO(int doctorID, string firstName, string lastName,
+            DateTime dateOfBirth, string phone, string email, string address,
+            string gender, string photoURL, int specializationID)
+        {
+            DoctorID = doctorID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Phone = phone;
+            Email = email;
+            Address = address;
+            Gender = gender;
+            PhotoURL = photoURL;
+            SpecializationID = specializationID;
+        }
     }
 }
