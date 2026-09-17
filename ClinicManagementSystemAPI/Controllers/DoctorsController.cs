ed2c2b805e649cdd79c8ff8cfb81b81063487ae1
WaelKendouli <kendouliwael@gmail.com>
2026-09-17 14:02:45 +0200

    I added the feature of adding new doctor

--- ClinicManagementSystemAPI/Controllers/DoctorsController.cs
+++ ClinicManagementSystemAPI/Controllers/DoctorsController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using DTOsLayer;
+using ClinicLogicLayer;
 namespace ClinicManagementSystemAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class DoctorsController : ControllerBase
     {
+        [HttpPost("AddNewDoctor" , Name = "AddNewDoctor")]
+        public async Task<IActionResult> PostDoctor([FromBody] DoctorDTO DTO)
+        {
+            clsDoctor NewDoctor = new clsDoctor(DTO.FirstName, DTO.LastName, DTO.DateOfBirth, DTO.Phone, DTO.Email,
+                DTO.Address, DTO.Gender,
+                DTO.PhotoURL, DTO.SpecializationID);
+            if (!await NewDoctor.Save())
+            {
+                return BadRequest("New Doctor was not added");
+            }
+            return Ok(DTO);
+        }
     }
 }
