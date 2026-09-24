c3a8f65937dfb97176d9c5cb74053e57e77bc7ff
WaelKendouli <kendouliwael@gmail.com>
2026-09-24 21:40:35 +0200

    adding get all patients method to the bussiness layer

---
+++ ClinicLogicLayer/clsPatient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicLogicLayer
+{
+    public class clsPatient
+    {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public clsPatient(int patientID, string firstName, string lastName,
+            DateTime dateOfBirth, string email, string phone)
+        {
+            PatientID = patientID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+            Phone = phone;
+        }
+
+        public static async Task<List<clsPatient>> GetAllPatientsAsListAsync()
+        {
+            return await clsPatient.GetAllPatientsAsListAsync();
+        }
+    }
+}
