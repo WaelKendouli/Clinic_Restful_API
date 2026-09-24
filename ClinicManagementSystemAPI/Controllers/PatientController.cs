c9bbe10c949b229865779460a428c29c39d62518
WaelKendouli <kendouliwael@gmail.com>
2026-09-24 21:47:59 +0200

    Adding on API level

---
+++ ClinicManagementSystemAPI/Controllers/PatientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClinicLogicLayer;
+
+namespace ClinicManagementSystemAPI.Controllers
+{
+    [Route("api/Patient")]
+    [ApiController]
+    public class PatientController : Controller
+    {
+        [HttpGet("GetAllPatients", Name = "GetAllPatients")]
+        public async Task<IActionResult> GetAllPatients()
+        {
+            var patients = await clsPatient.GetAllPatientsAsListAsync();
+            if (patients == null)
+            {
+                return BadRequest("Bad request from the client side");
+            }
+            return Ok(patients);
+        }
+    }
+}
