19bd322db90cc5ae8d1f9d7f25546e6a0d39befb
WaelKendouli <kendouliwael@gmail.com>
2026-09-24 21:35:21 +0200

    Adding data acces method + DTO

---
+++ DTOsLayer/PatientDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOsLayer
+{
+    public class PatientDTO
+    {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+
+        public PatientDTO(int patientID, string firstName, string lastName,
+            DateTime dateOfBirth, string email, string phone)
+        {
+            PatientID = patientID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
---
+++ DataAccessLayer/PatientsDA.cs
@@ -0,0 +1,57 @@
+﻿using DTOsLayer;
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DataAccessLayer
+{
+    public class PatientsDA
+    {
+
+        public static async Task<List<PatientDTO>> GetAllPatientsAsListAsync()
+        {
+            List<PatientDTO> patients = new List<PatientDTO>();
+            using (SqlConnection conx = new SqlConnection(clsConnection.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("SP_GetAllPatients", conx))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    await conx.OpenAsync();
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            // Handle nullable Email field
+                            var EmailOrdinal = reader.GetOrdinal("Email");
+                            string Email = reader.IsDBNull(EmailOrdinal) ? "" : reader.GetString(EmailOrdinal);
+
+                            PatientDTO patient = new PatientDTO(
+                                reader.GetInt32(reader.GetOrdinal("PatientID")),
+                                reader.GetString(reader.GetOrdinal("FirstName")),
+                                reader.GetString(reader.GetOrdinal("LastName")),
+                                reader.GetDateTime(reader.GetOrdinal("DateOfBirth")),
+                                Email,
+                                reader.GetString(reader.GetOrdinal("Phone"))
+                            );
+                            patients.Add(patient);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception (log it, etc.)
+                    return new List<PatientDTO>();
+                }
+            }
+            return patients;
+        }
+
+    }
+}
